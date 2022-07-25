d88c6b815f2d9e90b46bde1e734c9e087213a8bc
John Carlo Franco <francojohnc@gmail.com>
2022-07-25 13:05:29 +0800

    pwrdv

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import Queue from 'nq';
 
 
 
-Queue.setUrl('https://api.easybenta.com/v1');
+Queue.setUrl('https://api.pwrdv.com/v1');
 // Queue.setUrl('https://api.mweeb.com/v1');
 Queue.setApplicationId('6560588f36297abd70cb7433774d5e09');
 
