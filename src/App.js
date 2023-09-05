6901e4027465039863d134c337a502c07852e738
mweeb <mweebinc@gmail.com>
2023-09-05 13:24:32 +0800

    publish

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ import TenantPage from "./pages/TenantPage";
 // Queue.setUrl('https://api.innque.com/v1');
 // Queue.setUrl("http://188.166.177.35:8888/v1");
 // Queue.setUrl('https://api.figarocoffeegroup.com/v1');
-Queue.setUrl('https://api.test.figarocoffeegroup.com/v1');
+Queue.setUrl('https://api.zyllem.figarocoffeegroup.com/v1');
+// Queue.setUrl('https://api.test.figarocoffeegroup.com/v1');
 // Queue.setUrl('https://api.mercantiletravel.com/v1');
 // Queue.setApplicationId('DHQK1WV3bj');// cavite
 Queue.setApplicationId('6560588f36297abd70cb7433774d5e09');
